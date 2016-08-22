9395a7e5ed7f3bb4053945b495be962652af0bfb
Adam Stevenson <adam@stripe.com>
2016-08-22 15:23:50 -0700

    convert indentation to spaces -_-

--- php/authorize-card-full-example/authorize.php
+++ php/authorize-card-full-example/authorize.php
@@ -1,29 +1,29 @@
 <?php 
-	// Check for a POSTed stripeToken
-	if (isset($_POST['stripeToken'])){
-		try {
-			\Stripe\Customer::create(array(
-			  "description" => $_POST['stripeEmail'],
-			  "source" => $_POST['stripeToken'] // The token submitted from Checkout
-			));
-			$success = "Thanks! Your card has been authorized.";
-		}
-		catch(\Stripe\Error\Card $e) {
-		  // Since it's a decline, \Stripe\Error\Card will be caught
-		  $body = $e->getJsonBody();
-		  $error  = $body['error']['message'];
-		} catch (\Stripe\Error\RateLimit $e) {
-		  $error = "Sorry, we weren't able to authorize your card. Please try again.";
-		} catch (\Stripe\Error\InvalidRequest $e) {
-		  $error = "Sorry, we weren't able to authorize your card. Please try again.";
-		} catch (\Stripe\Error\Authentication $e) {
-		  $error = "Sorry, we weren't able to authorize your card. Please try again.";
-		} catch (\Stripe\Error\ApiConnection $e) {
-		  $error = "Sorry, we weren't able to authorize your card. Please try again.";
-		} catch (\Stripe\Error\Base $e) {
-		  $error = "Sorry, we weren't able to authorize your card. Please try again.";
-		} catch (Exception $e) {
-		  $error = "Sorry, we weren't able to authorize your card. Please try again.";
-		}
-	}
+  // Check for a POSTed stripeToken
+  if (isset($_POST['stripeToken'])){
+    try {
+      \Stripe\Customer::create(array(
+        "description" => $_POST['stripeEmail'],
+        "source" => $_POST['stripeToken'] // The token submitted from Checkout
+      ));
+      $success = "Thanks! Your card has been authorized.";
+    }
+    catch(\Stripe\Error\Card $e) {
+      // Since it's a decline, \Stripe\Error\Card will be caught
+      $body = $e->getJsonBody();
+      $error  = $body['error']['message'];
+    } catch (\Stripe\Error\RateLimit $e) {
+      $error = "Sorry, we weren't able to authorize your card. Please try again.";
+    } catch (\Stripe\Error\InvalidRequest $e) {
+      $error = "Sorry, we weren't able to authorize your card. Please try again.";
+    } catch (\Stripe\Error\Authentication $e) {
+      $error = "Sorry, we weren't able to authorize your card. Please try again.";
+    } catch (\Stripe\Error\ApiConnection $e) {
+      $error = "Sorry, we weren't able to authorize your card. Please try again.";
+    } catch (\Stripe\Error\Base $e) {
+      $error = "Sorry, we weren't able to authorize your card. Please try again.";
+    } catch (Exception $e) {
+      $error = "Sorry, we weren't able to authorize your card. Please try again.";
+    }
+  }
 ?>
